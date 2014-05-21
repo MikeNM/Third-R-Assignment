ed455591b1fb858e8be880b57accdc00b70da661
MikeNM <michael.maiale@yahoo.com>
2014-05-21 15:31:31 -0400

    add worst.R

---
+++ worst.R
@@ -0,0 +1,23 @@
+worst <- function(state, outcome){
+  fullData <- read.csv("outcome-of-care-measures.csv", colClasses= 'character')
+  if(!any(state == fullData$State)) {
+    stop('invalid state')
+  }
+  if(outcome == 'heart attack'){
+    i <- 11
+  }
+  else if(outcome == 'heart failure'){
+    i <- 17
+  }
+  else if(outcome == 'pneumonia'){
+    i <- 23
+  }
+  else {
+    stop('invalid outcome')
+  }
+  StateData <- fullData[fullData$State == state,]
+  StateData[,i] <- as.numeric(StateData[,i])
+  StateData <- StateData[complete.cases(StateData),]
+  NameWorst <- StateData[(StateData[,i] == max(StateData[,i])),]$Hospital.Name
+  NameWorst
+}
